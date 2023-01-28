b687fffbf74daf93ad365ecf437caacd14fc7f40
CarlosPizarroC <105620967+CarlosPizarroC@users.noreply.github.com>
2023-01-28 10:39:16 -0500

    Modificación de búsqueda mediante bucle FOR.
    Agregué el bucle FOR para que el código busque por si mismo la coincidencia de nombre requerida (Clementina DuBuque) y use su ID. Por intriga personal :p.

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const https = require('https');
 
-const userId = 10;
-const url = `https://jsonplaceholder.typicode.com/users/${userId}`;
-
 let city
 let catchPhrase
+var url = `https://jsonplaceholder.typicode.com/users/${userId}`;
 
-https.get(url, (res) => {
-    let data = '';
-    res.on('data', (chunk) => {
-        data += chunk;
+for (var userId=1; userId<=10; userId++){
+    url = `https://jsonplaceholder.typicode.com/users/${userId}`;
+    https.get(url, (res) => {
+        let data = '';
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            var user = JSON.parse(data);
+            if (user.name == "Clementina DuBuque"){
+                city = user.address["city"];
+                catchPhrase = user.company["catchPhrase"];
+                console.log(`City: ${city}`);
+                console.log(`CatchPhrase: ${catchPhrase}`);
+            }
+        });
+    }).on('error', (err) => {
+        console.log(`Error: ${err.message}`);
     });
-    res.on('end', () => {
-        const user = JSON.parse(data);
-        city = user.address["city"]
-        catchPhrase = user.company["catchPhrase"]
-        console.log(`City: ${city}`);
-        console.log(`CatchPhrase: ${catchPhrase}`);
-    });
-}).on('error', (err) => {
-    console.log(`Error: ${err.message}`);
-});
+}
+
 
 
 
